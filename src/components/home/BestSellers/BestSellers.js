d438eea8fe24818e9681513bd4353fe1593a8d63
jarvietnguyen318@gmail.com <jarvietnguyen318@gmail.com>
2024-03-13 16:57:18 +0700

    feat :call api get list product

--- src/components/home/BestSellers/BestSellers.js
+++ src/components/home/BestSellers/BestSellers.js
@@ -1,54 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Heading from "../Products/Heading";
 import Product from "../Products/Product";
-import {
-  bestSellerOne,
-  bestSellerTwo,
-  bestSellerThree,
-  bestSellerFour,
-} from "../../../assets/images/index";
+import { getListBestSeller } from "../../../constants/getListBestSeller";
 
 const BestSellers = () => {
+  const [bestSeller, setBestSellers] = useState([]);
+  //call api để lấy bestSellers
+  useEffect(() => {
+    //xai axios
+    getListBestSeller().then((res) => {
+      const { list_best_sellers_interior } = res;
+      // console.log(res);
+      // console.log(message);
+      // console.log(list_best_sellers_interior);
+      setBestSellers(list_best_sellers_interior);
+    });
+  }, []);
+
   return (
     <div className="w-full pb-20">
       <Heading heading="Our Bestsellers" />
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lgl:grid-cols-3 xl:grid-cols-4 gap-10">
-        <Product
-          _id="1011"
-          img={bestSellerOne}
-          productName="Flower Base"
-          price="35.00"
-          color="Blank and White"
-          badge={true}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
-        />
-        <Product
-          _id="1012"
-          img={bestSellerTwo}
-          productName="New Backpack"
-          price="180.00"
-          color="Gray"
-          badge={false}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
-        />
-        <Product
-          _id="1013"
-          img={bestSellerThree}
-          productName="Household materials"
-          price="25.00"
-          color="Mixed"
-          badge={true}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
-        />
-        <Product
-          _id="1014"
-          img={bestSellerFour}
-          productName="Travel Bag"
-          price="220.00"
-          color="Black"
-          badge={false}
-          des="Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic excepturi quibusdam odio deleniti reprehenderit facilis."
-        />
+        {bestSeller.map((product) => (
+          <Product
+            key={product._id}
+            _id={product._id}
+            img={product.images[0]}
+            productName={product.interior_name}
+            price={product.price}
+            color={product.color}
+            badge={true}
+            des={product.description}
+          />
+        ))}
       </div>
     </div>
   );
